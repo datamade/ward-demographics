5be0aaa94e58a05439eca8e53140ae6f01226ef8
Forest Gregg <fgregg@uchicago.edu>
2020-08-08 16:10:30 -0400

    dependencies fixed

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ wards.geojson :
 
 wards.csv : wards.geojson
 	cat $< | python scripts/demo.py | csvsort > $@
+
+precincts.geojson : municipal_general_2019.geojson
+	cat $< | python scripts/demo.py > $@
--- scripts/demo.py
+++ scripts/demo.py
@@ -1,8 +1,8 @@
-import csv
 import json
 import sys
 
 import scrapelib
+import tqdm
 from scrapelib.cache import FileCache
 from census_area import Census
 
@@ -14,7 +14,7 @@ s.cache_write_only = False
 
 API_KEY = 'ac94ba69718a7e1da4f89c6d218b8f6b5ae9ac49'
 
-wards = json.load(sys.stdin)
+geographies = json.load(sys.stdin)
 c = Census(API_KEY, session=s)
 
 VARS = {'B03002_001E': 0, # total population,
@@ -28,7 +28,7 @@ VARS = {'B03002_001E': 0, # total population,
 
 TRACT_LEVEL_VARS = {'B19001_001E', 'B25120_001E'}
 
-READABLE_VARS = {'ward number': None,
+READABLE_VARS = {'geography number': None,
                  'B03002_001E': 'Total Population',
                  'B03002_003E': 'Not Hispanic or Latino Origin, Whites',
                  'B03002_004E': 'Not Hispanic or Latino Origin, Blacks',
@@ -40,35 +40,36 @@ READABLE_VARS = {'ward number': None,
                  'number of tracts': None,
                  'number of blockgroups': None}
 
-writer = csv.DictWriter(sys.stdout, fieldnames=READABLE_VARS)
+for geography in tqdm.tqdm(geographies['features']):
+    geography_data = VARS.copy()
+    geography_geo = geography['geometry']
 
-writer.writerow({k: (v + '; ' + k) if v else k for k, v in READABLE_VARS.items()})
-
-for ward in wards['features']:
-    ward_data = VARS.copy()
-    ward_geo = ward['geometry']
-
-    blockgroups = c.acs5.geo_blockgroup(('NAME',) + tuple(VARS), ward_geo)
+    blockgroups = c.acs5.geo_blockgroup(('NAME',) + tuple(VARS), geography_geo)
     total_blockgroups = 0
     for geojson, data, weight in blockgroups:
         for var in (VARS.keys() - TRACT_LEVEL_VARS):
-            ward_data[var] += (data[var] * weight)
+            geography_data[var] += (data[var] * weight)
         total_blockgroups += weight
 
-    tracts = c.acs5.geo_tract(('NAME',) + tuple(VARS), ward_geo)
+    tracts = c.acs5.geo_tract(('NAME',) + tuple(VARS), geography_geo)
     total_tracts = 0
     for geojson, data, weight in tracts:
         if data['B25120_001E'] is None:
             data['B25120_001E'] = 0
             data['B19001_001E'] = 0           
         for var in TRACT_LEVEL_VARS:
-            ward_data[var] += (data[var] * weight)
+            geography_data[var] += (data[var] * weight)
         total_tracts += weight 
 
-    ward_data['mean household income'] = ward_data['B25120_001E'] / ward_data['B19001_001E']
-    ward_data = {k: int(v) for k, v in ward_data.items()}
-    ward_data['ward number'] = ward['properties']['ward']
-    ward_data['number of tracts'] = total_tracts
-    ward_data['number of blockgroups'] = total_blockgroups
+    try:
+        geography_data['mean household income'] = geography_data['B25120_001E'] / geography_data['B19001_001E']
+    except ZeroDivisionError:
+        geography_data['mean household income'] = None
+        
+    geography_data = {k: int(v) for k, v in geography_data.items() if v is not None}
+    geography_data['number of tracts'] = total_tracts
+    geography_data['number of blockgroups'] = total_blockgroups
 
-    writer.writerow(ward_data)
+    geography['properties'].update(geography_data)
+
+json.dump(geographies, sys.stdout)
